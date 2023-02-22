2312ab5e8d9e0445c1ec83efe2e9c631705c1d19
Shubham Singh <shubham2101@gmail.com>
2023-02-22 09:26:39 +0530

    removing light mode

--- app.js
+++ app.js
@@ -7,9 +7,6 @@
       document.getElementById(button.dataset.id).classList.add("active");
     });
   });
-  document.querySelector(".theme-btn").addEventListener("click", () => {
-    document.body.classList.toggle("light-mode");
-  });
 })();
 
 // <!-- typed js effect  -->
@@ -22,16 +19,17 @@ var typed = new Typed(".typed-text", {
 });
 
 // emailjs contact form
-function SendMail() { 
-
-  let params  = {
-    from_name : document.getElementById("fullName").value,
-    email_id : document.getElementById("email_id").value,
-    subject : document.getElementById("subject").value,
-    message : document.getElementById("message").value,
-  }
-  emailjs.send("service_xo5zroe", "template_cnc3pqr", params).then(function(res) {
-    alert("Message sent successfully!" + res.status)
-  })
+function SendMail() {
+  let params = {
+    from_name: document.getElementById("fullName").value,
+    email_id: document.getElementById("email_id").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs
+    .send("service_xo5zroe", "template_cnc3pqr", params)
+    .then(function (res) {
+      alert("Message sent successfully!" + res.status);
+    });
   document.querySelector(".contact-form").reset();
 }
